43979a500e97a7fb61ef47a6766815781db2daea
Ben Howlett <ben@benhowlett.com>
2023-01-21 13:42:50 -0700

    Initial Commit

---
+++ MyIdleGame/MyIdleGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyIdleGameApp.swift
+//  MyIdleGame
+//
+//  Created by Ben Howlett on 2023-01-21.
+//
+
+import SwiftUI
+
+@main
+struct MyIdleGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
